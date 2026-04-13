dd9f99c97531a0c6b1d43ab57ac7bd7079adc790
martinvesely <imthemartinveselyap@gmail.com>
2026-04-13 18:55:33 +0200

    Add Material-UI dependencies and implement basic layout in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
+import { Container } from '@mui/material';
 import './App.css';
+import CssBaseline from '@mui/material/CssBaseline';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+
+const theme = createTheme();
 
 function App() {
-  return <></>;
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline enableColorScheme />
+      <Container
+        maxWidth={false}
+        sx={{ width: '95%', maxWidth: '1200px', marginInline: 'auto' }}
+        disableGutters
+      ></Container>
+    </ThemeProvider>
+  );
 }
 
 export default App;
